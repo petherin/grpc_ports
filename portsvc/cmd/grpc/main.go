5a589443777d276751ebf71caad899f71986f2de
Peter Etherington <Peter.Etherington@skybettingandgaming.com>
2022-03-27 11:25:42 +0100

    Create gRPC service, add Makefile for convenience. Can't really this test this yet, part from using Postman's new gRPC capability, which actually works

---
+++ portsvc/cmd/grpc/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/petherin/grpc_ports/proto"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	const defaultPort = ":50051"
+
+	lis, err := net.Listen("tcp", defaultPort)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+
+	grpcServer := grpc.NewServer(opts...)
+
+	//TODO replace later, pass in test list of ports
+	testList := map[string]proto.Port{
+		"AEAJM": {
+			Name:    "Ajman",
+			City:    "Ajman",
+			Country: "United Arab Emirates",
+			Alias:   nil,
+			Regions: nil,
+			Coordinates: []float32{
+				55.5136433,
+				25.4052165,
+			},
+			Timezone: "Asia/Dubai",
+			Unlocs:   []string{"AEAJM"},
+			Code:     "52000",
+		},
+	}
+
+	proto.RegisterPortsServer(grpcServer, proto.NewService(testList))
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
--- readme.md
+++ readme.md
@@ -2,5 +2,4 @@
 
 A gRPC service to store port data.
 
-Ad a client api to read in a json file and save to the service.
-
+And a client api to read in a json file and save to the service.
